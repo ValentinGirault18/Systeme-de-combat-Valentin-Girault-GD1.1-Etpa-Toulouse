39a0bf2b080455c7c2df36c595c3cafa641c2aa3
ValentinGirault18 <vgirault.nc18@gmail.com>
2020-01-18 22:11:02 +0100

    Update Systeme_de_combat_Valentin_GIRAULT_GD1_1.c

--- Systeme_de_combat_Valentin_GIRAULT_GD1_1.c
+++ Systeme_de_combat_Valentin_GIRAULT_GD1_1.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 
+int PDV=100; 
+
+int attaqueSimple (int PDVcible){
+	
+	PDVcible=PDVcible-10;
+	return PDVcible;
+}
+
 int main(){
 	
+	printf ("Vous avez %d points de vie \n",PDV);
+	printf ("Vous subissez une attaque! -10 points de vie \n");
+	PDV=attaqueSimple(PDV);
+	printf ("Vous avez %d points de vie \n",PDV);
 	
 	
 	return 0;
