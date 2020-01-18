27799add586bc83499b6ab4ceef5580bcf870fb7
ValentinGirault18 <vgirault.nc18@gmail.com>
2020-01-18 23:26:19 +0100

    Update Systeme_de_combat_Valentin_GIRAULT_GD1_1.c

--- Systeme_de_combat_Valentin_GIRAULT_GD1_1.c
+++ Systeme_de_combat_Valentin_GIRAULT_GD1_1.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-int PDVmonstre=100; 
+int PDVmonstre=150; 
+int PDVjoueur=100;
 int ChoixAttaque=0;
 
 
@@ -12,10 +13,13 @@ int attaqueSimple (int PDVcible){
 
 int main(){
 	
-	
+	printf("Vous avez %d points de vie \n",PDVjoueur); 
+	printf("Un monstre apparait! Il a %d points de vie! \n",PDVmonstre); 
 	
 	while (PDVmonstre>1){
-		printf(" taper (1) pour attaquer/ (2) pour ce defendre/ (3) pour empoisonner \n");
+		
+		
+		printf(" taper (1) pour attaquer/ (2) pour vous defendre/ (3) pour empoisonner \n");
 		scanf("%d",&ChoixAttaque);
 		while(ChoixAttaque!=1){
 			printf("l'attaque numero %d n'existe pas! \n",ChoixAttaque);
@@ -27,6 +31,10 @@ int main(){
 			PDVmonstre=attaqueSimple(PDVmonstre);
 			printf ("Le monstre a %d points de vie \n",PDVmonstre);
 			}
+			
+		printf("Le monstre vous attaque! Vous subissez -10 points de vie! \n \n");
+		PDVjoueur=attaqueSimple(PDVjoueur);
+		printf ("Vous avez %d points de vie \n",PDVjoueur);
 	}
 	
 	
