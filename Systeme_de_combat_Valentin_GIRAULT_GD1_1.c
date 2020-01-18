eb331b87c151b1a052dd21246bd3c88956b44597
ValentinGirault18 <vgirault.nc18@gmail.com>
2020-01-19 00:39:02 +0100

    Update Systeme_de_combat_Valentin_GIRAULT_GD1_1.c

--- Systeme_de_combat_Valentin_GIRAULT_GD1_1.c
+++ Systeme_de_combat_Valentin_GIRAULT_GD1_1.c
@@ -1,49 +1,76 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int PDVmonstre=300; 
 int PDVjoueur=200;
 int ChoixAttaque=0;
-int degat=20;
+int ChoixMonstre=0;
+int degatJoueur=20;
+int degatMonstre=20;
 
-int attaqueSimple (int PDVcible){
+int attaqueJoueur (int PDVciblej){
 	
-	PDVcible=PDVcible-degat;
-	return PDVcible;
+	PDVciblej=PDVciblej-degatJoueur;
+	return PDVciblej;
 }
+int attaqueMonstre (int PDVcibleM){
+	
+	PDVcibleM=PDVcibleM-degatMonstre;
+	return PDVcibleM;
+}
+
 
 int main(){
 	
 	printf("Vous avez %d points de vie \n",PDVjoueur); 
 	printf("Un monstre apparait! Il a %d points de vie! \n",PDVmonstre); 
 	
-	while (PDVmonstre>1){
+	while (PDVmonstre>1 || PDVjoueur>1){
 		
 		
 		printf(" taper (1) pour attaquer/ (2) pour vous defendre/ (3) pour empoisonner \n");
 		scanf("%d",&ChoixAttaque);
 		while(ChoixAttaque!=1 && ChoixAttaque!=2 && ChoixAttaque!=3 ){
 			printf("l'attaque numero %d n'existe pas! \n",ChoixAttaque);
-			scanf ("%d taper (1) pour attaquer/ (2) pour ce defendre/ (3) pour empoisonner \n",&ChoixAttaque);
+			printf (" taper (1) pour attaquer/ (2) pour ce defendre/ (3) pour empoisonner \n");
+			scanf ("%d",&ChoixAttaque);
 		}
 		if (ChoixAttaque==1){
 			printf ("Le monstre a %d points de vie \n",PDVmonstre);
-			printf ("Vous attaquez a l'aide de votre katana! le monstre subie %d points de vie \n",degat);
-			PDVmonstre=attaqueSimple(PDVmonstre);
+			printf ("Vous attaquez a l'aide de votre katana! le monstre subie %d points de vie \n",degatMonstre);
+			PDVmonstre=attaqueMonstre(PDVmonstre);
 			printf ("Le monstre a %d points de vie \n",PDVmonstre);
 			}
 		if (ChoixAttaque==2){
 			printf ("Le monstre a %d points de vie \n",PDVmonstre);
 			printf ("Vous vous defendez! Vous subirez moins de degats lors de la prochaine attaque du monstre \n \n");
-			degat=5;
+			degatJoueur=5;
 			
 		}
+		degatMonstre=20;
 		
-		printf("Le monstre vous attaque! Vous subissez %d points de vie! \n",degat);
-		PDVjoueur=attaqueSimple(PDVjoueur);
-		printf ("Vous avez %d points de vie \n",PDVjoueur);
-		degat=20;
+		ChoixMonstre = rand()%2+1;
+		
+		if (ChoixMonstre==1){
+			printf("Le monstre vous attaque! Vous subissez %d points de vie! \n",degatJoueur);
+			PDVjoueur=attaqueJoueur(PDVjoueur);
+			printf ("Vous avez %d points de vie \n",PDVjoueur);
+			degatJoueur=20;
+	    }
+		if(ChoixMonstre==2){
+			printf("Le monstre ce defend, il subira moins de degat au prochain tour \n");
+			printf ("Vous avez %d points de vie \n",PDVjoueur);
+			degatMonstre=5;
+		}
+		
 	}
 	
+	if (PDVmonstre<0){
+		printf("Victoire!");
+	}
+	if(PDVjoueur<0){
+		printf("Vous etes mort!");
+	}
 	
 	return 0;
 }
